453f21b48771e86531fc15c5d621029694e167c6
宋琦琦 <qiqiorz@qq.com>
2020-12-08 11:27:27 +0800

    添加项目文件。

---
+++ TextMerge/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace TextMerge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChoose = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.lblLog = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // btnChoose
+            // 
+            this.btnChoose.Location = new System.Drawing.Point(310, 10);
+            this.btnChoose.Name = "btnChoose";
+            this.btnChoose.Size = new System.Drawing.Size(75, 23);
+            this.btnChoose.TabIndex = 0;
+            this.btnChoose.Text = "选择文件夹";
+            this.btnChoose.UseVisualStyleBackColor = true;
+            this.btnChoose.Click += new System.EventHandler(this.btnChoose_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(12, 12);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(292, 21);
+            this.txtPath.TabIndex = 1;
+            // 
+            // lblLog
+            // 
+            this.lblLog.AutoSize = true;
+            this.lblLog.Location = new System.Drawing.Point(12, 44);
+            this.lblLog.Name = "lblLog";
+            this.lblLog.Size = new System.Drawing.Size(0, 12);
+            this.lblLog.TabIndex = 2;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(310, 39);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "开始导出";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 75);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblLog);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnChoose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "文本合并";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChoose;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label lblLog;
+        private System.Windows.Forms.Button btnStart;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ TextMerge/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace TextMerge
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public List<string> Paths;
+        public StringBuilder Sb;
+
+        private void btnChoose_Click(object sender, EventArgs e)
+        {
+            Paths = new List<string>();
+            Sb = new StringBuilder();
+            lblLog.Text = string.Empty;
+            btnStart.Enabled = true;
+            var dialog = folderBrowserDialog1.ShowDialog();
+            if (dialog == DialogResult.OK)
+            {
+                txtPath.Text = folderBrowserDialog1.SelectedPath;
+                GetDirectory(txtPath.Text);
+                lblLog.Text = $"解析成功，共找到{Paths.Count}个文件。";
+            }
+        }
+
+        public void GetFileName(string path)
+        {
+            DirectoryInfo root = new DirectoryInfo(path);
+            foreach (FileInfo f in root.GetFiles("*.txt"))
+            {
+                Paths.Add(f.FullName);
+            }
+        }
+
+        public void GetDirectory(string path)
+        {
+            GetFileName(path);
+            DirectoryInfo root = new DirectoryInfo(path);
+            foreach (DirectoryInfo d in root.GetDirectories())
+            {
+                GetDirectory(d.FullName);
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+            btnStart.Enabled = false;
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 1; i <= Paths.Count; i++)
+            {
+                var path = Paths[i - 1];
+                backgroundWorker1.ReportProgress(i);
+                Sb.Append(File.ReadAllText(path, Encoding.UTF8));
+            }
+            File.WriteAllText("D:\\output.txt", Sb.ToString(), Encoding.UTF8);
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            lblLog.Text = $"正在导出第{e.ProgressPercentage}/{Paths.Count}个， D:\\output.txt。";
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            lblLog.Text = $"导出完成，路径在 D:\\output.txt。";
+            btnStart.Enabled = true;
+        }
+    }
+}
